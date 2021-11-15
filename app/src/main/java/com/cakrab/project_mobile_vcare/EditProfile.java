3bd427d717b6274607d7a4fd533aa732733b79f7
alvinpurnamaa <alvinpurnama26@gmail.com>
2021-11-15 15:03:56 +0700

    Edit Profile

---
+++ app/src/main/java/com/cakrab/project_mobile_vcare/EditProfile.java
@@ -0,0 +1,12 @@
+package com.cakrab.project_mobile_vcare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+    }
+}
---
+++ app/src/main/java/com/cakrab/project_mobile_vcare/Profile.java
@@ -0,0 +1,28 @@
+package com.cakrab.project_mobile_vcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity {
+
+    Button btnEditProfile;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        btnEditProfile=findViewById(R.id.btnEditProfile);
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, EditProfile.class );
+                startActivity(intent);
+            }
+        });
+    }
+}
