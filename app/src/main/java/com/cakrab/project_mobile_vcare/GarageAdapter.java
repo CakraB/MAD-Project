a26514d751721d676bd07d6393f102e7aa79b403
euricov14 <eurico.val@gmail.com>
2021-12-08 12:57:11 +0700

    GarageAdapter

--- app/src/main/java/com/cakrab/project_mobile_vcare/GarageAdapter.java
+++ app/src/main/java/com/cakrab/project_mobile_vcare/GarageAdapter.java
@@ -1,31 +1,10 @@
 package com.cakrab.project_mobile_vcare;
 
-import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+public class GarageAdapter {
 
-public class GarageAdapter extends RecyclerView.Adapter<GarageAdapter.ViewHolder> {
-    public GarageAdapter(@NonNull Context context) {
-        super(context);
-    }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 
 }
