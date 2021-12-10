1e6b95ee8407b017a57f437081a60f2966ffe7d1
CakraB <cakrabuana7255@gmail.com>
2021-12-10 14:58:50 +0700

    Refactor Profile and Fix Some Issus

--- app/src/main/java/com/cakrab/project_mobile_vcare/ProfileFragment.java
+++ app/src/main/java/com/cakrab/project_mobile_vcare/ProfileFragment.java
@@ -4,18 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import org.jetbrains.annotations.NotNull;
 
 public class ProfileFragment extends Fragment {
-    TextView textViewProfile;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -27,18 +23,5 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        clickViewProfile();
-    }
-
-    private void clickViewProfile() {
-        textViewProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment fragment = new NewsFragment();
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, fragment).commit();
-            }
-        });
     }
 }
