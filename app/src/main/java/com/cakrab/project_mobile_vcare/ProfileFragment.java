b30fce6f6edd732689b0e75c3fa25cddbaa37869
CakraB <cakrabuana7255@gmail.com>
2021-12-13 12:11:07 +0700

    Adding SharedPreferences on Profile

--- app/src/main/java/com/cakrab/project_mobile_vcare/ProfileFragment.java
+++ app/src/main/java/com/cakrab/project_mobile_vcare/ProfileFragment.java
@@ -1,9 +1,14 @@
 package com.cakrab.project_mobile_vcare;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +17,10 @@ import androidx.fragment.app.Fragment;
 import org.jetbrains.annotations.NotNull;
 
 public class ProfileFragment extends Fragment {
+
+    TextView textProfileName, textProfileEmail;
+    Button btnEditProfile, btnLogout;
+    SharedPreferences sharedPreferences;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -23,5 +32,35 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        textProfileName = view.findViewById(R.id.text_profile_name);
+        textProfileEmail = view.findViewById(R.id.text_profile_email);
+        btnEditProfile = view.findViewById(R.id.btnEditProfile);
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        sharedPreferences = this.getActivity().getSharedPreferences("REMEMBER", Context.MODE_PRIVATE);
+        String getData = sharedPreferences.getString("EMAIL", "");
+        textProfileName.setText(getData);
+        textProfileEmail.setText(getData);
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+
+                Intent logout = new Intent(getContext(), LoginActivity.class);
+                startActivity(logout);
+            }
+        });
     }
 }
