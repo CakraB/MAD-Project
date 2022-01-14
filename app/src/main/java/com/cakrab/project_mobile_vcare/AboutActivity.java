b71d896e3c89fd4acc76eb28856879117db934aa
alvinpurnamaa <alvinpurnama26@gmail.com>
2022-01-15 01:33:27 +0700

    Edit About Us Activity

--- app/src/main/java/com/cakrab/project_mobile_vcare/AboutActivity.java
+++ app/src/main/java/com/cakrab/project_mobile_vcare/AboutActivity.java
@@ -1,14 +1,42 @@
 package com.cakrab.project_mobile_vcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AboutActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class AboutActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        init();
+
+    }
+
+    public void init () {
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this::onMapReady);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng BnKmg = new LatLng(-6.201733, 106.7815);
+        map.addMarker(new MarkerOptions().position(BnKmg).title("VCare"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(BnKmg));
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+
     }
 }
